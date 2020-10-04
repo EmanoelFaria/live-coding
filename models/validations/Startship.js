e90c0b95590965cc0e3668affccff0ac6b469a59
Emanoel Faria <emanoel-faria@hotmail.com>
2020-10-04 00:24:25 -0300

    feat: starship dto middleware validation and route

---
+++ models/validations/Startship.js
@@ -0,0 +1,9 @@
+const yup = require('yup');
+
+module.exports = yup.object().shape({
+  name: yup.string().required(),
+  model: yup.string().required(),
+  manufacturer: yup.string().required(),
+  passengers: yup.number().positive().required(),
+  pilotsIds: yup.array().of(yup.number()),
+});
---
+++ routes/starship.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const StarshipController = require('../controllers/StarshipsController');
+const { DTO } = require('../middlewares/dto');
+const starshipBodyValidation = require('../models/validations/Startship');
+
+router.get('/', StarshipController.getAll);
+router.post('/', DTO.validate(starshipBodyValidation), StarshipController.createOne);
+
+module.exports = router;
