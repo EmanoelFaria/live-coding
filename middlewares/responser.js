6159a670cbc28f6fdcf40f1513d4112ffe7c7c9e
Emanoel Faria <emanoel-faria@hotmail.com>
2020-10-04 16:51:29 -0300

    feat: add log for general erros for dev

--- middlewares/responser.js
+++ middlewares/responser.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next) => {
   res.responser = (status, message = '', data = {}, error = null, type = 'json') => {
     if (error) {
+      if (process.env.NODE_ENV == 'development') console.log(error);
       return res.status(status).type(type).send({
         error: {
           status,
