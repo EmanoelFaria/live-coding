9079006700de38e88a9845cd0d4f3df70fdab42e
Emanoel Faria <emanoel-faria@hotmail.com>
2020-10-04 15:37:55 -0300

    fix: remove dto import from starship router

--- routes/starship.js
+++ routes/starship.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const StarshipController = require('../controllers/StarshipsController');
-const { DTO } = require('../middlewares/dto');
-const starshipBodyValidation = require('../models/validations/Startship');
 
 router.get('/', StarshipController.getAll);
 router.post('/', StarshipController.createOne);
