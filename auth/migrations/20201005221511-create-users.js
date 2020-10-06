48ba0764409f635499bb18ae0e08f760189eae23
Emanoel Faria <emanoel-faria@hotmail.com>
2020-10-06 02:47:52 -0300

    feat: initial structure and configuration for auth micro service

---
+++ auth/migrations/20201005221511-create-users.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      clientId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      ip: {
+        type: Sequelize.STRING,
+      },
+      token: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      accessToken: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  },
+};
